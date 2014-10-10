91e73cdafe8ef084531d0c159339af18068423da
Vladimir Bodnartchouk <vladimir.bodnartchouk@activeeon.com>
2014-10-10 17:25:26 +0200

    added tests, removed manual testing
    Change-Id: I425fb55a332d11b9dfe310951c1114c63d549332

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26808ea0-796b-4203-8276-4b1a376a2ddd")]
+[assembly: Guid("793672c4-621a-4bc0-a4f4-b571494fe0c3")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Tests/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpRestClient;
+
+namespace Tests
+{
+    [TestClass]
+    public class RestClientTests
+    {
+        public static readonly string REST_SERVER_URL = "http://localhost:8080/rest";        
+
+        public static SchedulerClient sc;
+
+        [ClassInitialize]
+        public static void BeforeAll(TestContext ctx)
+        {
+            Console.WriteLine("----------------CONNECTING TO THE SCHEDULER---------------");
+            sc = SchedulerClient.connect(REST_SERVER_URL, "admin", "admin");            
+        }
+
+        [ClassCleanup]
+        public static void AfterAll()
+        {
+            Console.WriteLine("----------------HLKJH---------------");
+        }
+
+        [TestMethod]
+        public void TestGetStatus()
+        {
+            Console.WriteLine("Scheduler status: " + sc.GetStatus());
+
+            Assert.AreEqual<SchedulerStatus>(SchedulerStatus.STARTED, sc.GetStatus());
+        }
+
+        [TestMethod]
+        public void TestSubmitXml()
+        {
+            JobId jid = sc.SubmitXml(@"C:\tmp\ProActiveWorkflowsScheduling-windows-x64-6.0.1\samples\workflows\01_simple_task.xml");
+            
+            //Console.WriteLine("---> jobid.id = " + jid.Id);
+            //Console.WriteLine("---> jobid.ReadableName = " + jid.ReadableName);
+
+            Assert.AreNotEqual<long>(0, jid.Id);
+            Assert.AreEqual<string>("01_simple_task", jid.ReadableName);
+
+            sc.GetJobState(jid);
+
+            System.Threading.Thread.Sleep(5000);
+
+            Assert.AreEqual<bool>(false, sc.PauseJob(jid));
+            Assert.AreEqual<bool>(false, sc.ResumeJob(jid));
+            Assert.AreEqual<bool>(true, sc.KillJob(jid));
+            Assert.AreEqual<bool>(true, sc.RemoveJob(jid));
+        }
+
+
+    }
+}
