6b264ba9387ec8809778917bb194958b1233fad5
Vladimir Bodnartchouk <vladimir.bodnartchouk@activeeon.com>
2014-10-14 19:07:58 +0200

    update tests and references
    Change-Id: I4e448aea9fddb4b8048d615fdbccb99f3d1a933b

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,77 +1,103 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpRestClient;
+using System.IO;
 
 namespace Tests
 {
     [TestClass]
     public class RestClientTests
     {
-        public static readonly string LOCAL_REST_SERVER_URL = "http://localhost:8080/restt";
+        public static readonly string LOCAL_REST_SERVER_URL = "http://localhost:8080/rest";
 
         public static readonly string TRY_REST_SERVER_URL = "https://try.activeeon.com/rest";
 
-        public static SchedulerClient sc;
+        public static SchedulerClient sc;        
 
         [ClassInitialize]
         public static void BeforeAll(TestContext ctx)
         {
             try
             {
-                Console.WriteLine("--- Trying to connect to " + LOCAL_REST_SERVER_URL);
-                sc = SchedulerClient.connect(LOCAL_REST_SERVER_URL, "admin", "admin");
+                Console.WriteLine("Trying to connect to " + LOCAL_REST_SERVER_URL);
+                sc = SchedulerClient.Connect(LOCAL_REST_SERVER_URL, "admin", "admin");
             }
             catch (Exception)
             {
-                Console.WriteLine("--- !!! No Scheduler running on localhost !!! ... Trying to connect to " + TRY_REST_SERVER_URL);
+                Console.WriteLine("No Scheduler running on localhost, trying to connect to " + TRY_REST_SERVER_URL);
                 try
                 {
-                    sc = SchedulerClient.connect(TRY_REST_SERVER_URL, "demo", "*****");
+                    string p = Environment.GetEnvironmentVariable("TRY_DEMO_PASS", EnvironmentVariableTarget.Machine);
+                    sc = SchedulerClient.Connect(TRY_REST_SERVER_URL, "demo", p);
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("--- !!! No Scheduler running on try !!! ... Unable to run the tests");
+                    Assert.Fail("Unable to run tests! There is no scheduler running on try!");
                 }
+            }
+
+            // Check Scheduler Verison is 6.X
+            SharpRestClient.Version ver = sc.GetVersion();
+            Assert.IsNotNull(ver.Scheduler);
+            Assert.IsNotNull(ver.Rest);            
+            if (!ver.Scheduler.StartsWith("6"))
+            {
+                Assert.Fail("The Scheduler version is not 6.X");
+            }
+            if (!ver.Rest.StartsWith("6"))
+            {
+                Assert.Fail("The Rest version is not 6.X");
             }
         }
 
         [ClassCleanup]
         public static void AfterAll()
         {
-            Console.WriteLine("----------------ClassCleanup---------------");
+            //Console.WriteLine("----------------ClassCleanup---------------");
         }
 
         [TestMethod]
-        public void TestGetStatus()
+        public void IsConnected()
         {
-            Console.WriteLine("Scheduler status: " + sc.GetStatus());
+            Assert.AreEqual<bool>(true, sc.IsConnected());
+        }
 
+        [TestMethod]
+        public void GetSchedulerStatus()
+        {            
             Assert.AreEqual<SchedulerStatus>(SchedulerStatus.STARTED, sc.GetStatus());
         }
 
         [TestMethod]
-        public void TestSubmitXml()
+        public void PushPullFile()
+        {
+            // Create temp file in temp dir
+            string tempFilePath = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(tempFilePath);
+            try
+            {
+                bool ok = sc.PushFile("GLOBALSPACE", "", fileInfo.Name, tempFilePath);
+                Assert.IsTrue(ok);
+            }
+            finally
+            {
+                fileInfo.Delete();
+            }
+        }
+
+        [TestMethod]
+        public void SubmitXml()
         {
             JobId jid = sc.SubmitXml(@"C:\tmp\ProActiveWorkflowsScheduling-windows-x64-6.0.1\samples\workflows\01_simple_task.xml");
-
-            Console.WriteLine("---> jobid.id = " + jid.Id);
-            //Console.WriteLine("---> jobid.ReadableName = " + jid.ReadableName);
-
             Assert.AreNotEqual<long>(0, jid.Id);
             Assert.AreEqual<string>("01_simple_task", jid.ReadableName);
-
-            Console.WriteLine("---> asking job state ...");
-            JobState jobState = sc.GetJobState(jid);
-            Console.WriteLine("---> GOT: " + jobState);
-
+            Assert.AreEqual<bool>(true, sc.isJobAlive(jid));
+            //JobState jobState = sc.GetJobState(jid);            
             System.Threading.Thread.Sleep(5000);
-
             Assert.AreEqual<bool>(false, sc.PauseJob(jid));
             Assert.AreEqual<bool>(false, sc.ResumeJob(jid));
             Assert.AreEqual<bool>(true, sc.KillJob(jid));
             Assert.AreEqual<bool>(true, sc.RemoveJob(jid));
         }
-
-
     }
 }
